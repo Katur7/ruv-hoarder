68b557628927d38854f260cbfedff9af6b2ae699
Grímur Kristinsson <grimurk@mgmail.com>
2024-01-09 22:58:47 +0100

    Add module for downloading tv shows

---
+++ tv.js
@@ -0,0 +1,21 @@
+import { readFileSync } from 'fs';
+import { downloadEpisodes,
+         listAvailableEpisodes,
+         listDownloadedEpisodes,
+         listMissingEpisodes } from './shared/tv-utils.js';
+
+const showsConfig = JSON.parse(readFileSync('./config/tv-shows.json'));
+
+for(const showConfig of showsConfig.filter(s => s.download)) {
+    console.log('##### Starting to check and download ' + showConfig.title);
+    const availableEpisodes = await listAvailableEpisodes(showConfig);
+    const downloadedEpisodes = listDownloadedEpisodes(showConfig);
+    const missingEpisodes = listMissingEpisodes(availableEpisodes, downloadedEpisodes);
+
+    await downloadEpisodes(missingEpisodes, showConfig);
+    await delay(300);
+}
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
